f21a3f876800e7f5a29bbd992a721de3a0cae9ed
Aigulth <aigul_th@mail.ru>
2019-06-09 13:32:27 +0500

    Task1 calc

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите а: ");
+    int a = in.nextInt();
+    System.out.print("Введите б: ");
+    int b = in.nextInt();
+    System.out.print("Введите операцию: ");
+    char oper = in.next().charAt(0);
+    int rez=0;
+    switch (oper) {
+      case '+':
+        rez = a + b;
+        break;
+      case '-':
+        rez = a - b;
+        break;
+      case '*':
+        rez = a * b;
+        break;
+      case '/':
+        rez = a / b;
+        break;
+    }
+    System.out.println(a + "+" + b + "=" + rez);
+    in.close();
   }
 }
